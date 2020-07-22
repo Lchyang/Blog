7300f1679c6e56e66a7be92a3013e201ef43edf8
lichy <lichunyangcoder@gmail.com>
2020-07-22 13:12:45 +0800

    部署前配置文件修改

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2pii#c@oh@*gbi@z4r#d8z_(t($*$9uvxh2_99u4fun9oe6cuv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
